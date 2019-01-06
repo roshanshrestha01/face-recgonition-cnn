56f4df4ed6d90f786c44ce72748a283f776f7425
Roshan Shrestha <roshanshrestha01@gmail.com>
2019-01-06 13:08:24 +0545

    prepare dataset in custom ratio

--- prepare.py
+++ prepare.py
@@ -1,4 +1,9 @@
+import os
 import argparse
+import shutil
+import cv2
+from settings import PROCESSED_DIR, RAW_DIR
+from utils import check_folder
 
 parser = argparse.ArgumentParser(description='Data sample parameters.')
 
@@ -7,14 +12,53 @@ parser.add_argument('ratio', metavar='N', type=int, nargs='+',
 
 args = parser.parse_args()
 
-if len(args.ratio) == 2:
-    datasets = ['train', 'test']
-elif len(args.ratio) == 3:
-    datasets = ['train', 'validate', 'test']
+is_two_split = len(args.ratio) == 2
+is_three_split = len(args.ratio) == 3
+
+validate = 0
+if is_two_split:
+    train = args.ratio[0]
+    test = args.ratio[1]
+    data_sets = ['train', 'test']
+elif is_three_split:
+    train = args.ratio[0]
+    validate = args.ratio[1]
+    test = args.ratio[2]
+    data_sets = ['train', 'validate', 'test']
 else:
     raise ValueError('Ratio should be 2 or 3 values i.e. training and testing or training, validation and testing')
 
 if not sum(args.ratio) == 10:
     raise ValueError('Sum of ratio should be equal 10')
 
+if os.path.exists(PROCESSED_DIR):
+    shutil.rmtree(PROCESSED_DIR)
+check_folder(PROCESSED_DIR)
 
+subjects = []
+for root, dirnames, files in os.walk(RAW_DIR):
+    for dirname in dirnames:
+        subjects.append(dirname)
+
+for subject in subjects:
+    for root, dirName, files in os.walk(os.path.join(RAW_DIR, subject)):
+        files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+        if is_two_split:
+            training_sets = files[:train]
+            testing_sets = files[train:]
+            data = [training_sets, testing_sets]
+        else:
+            training_sets = files[:train]
+            other_half = files[train:]
+            validating_sets = other_half[:validate]
+            testing_sets = other_half[validate:]
+            data = [training_sets, validating_sets, testing_sets]
+
+        for param in zip(data_sets, data):
+            check_folder(os.path.join(PROCESSED_DIR, param[0]))
+            check_folder(os.path.join(PROCESSED_DIR, param[0], subject))
+            DUMP_DIR = os.path.join(PROCESSED_DIR, param[0], subject)
+            for image_filename in param[1]:
+                image_path = os.path.join(root, image_filename)
+                image = cv2.imread(image_path, 0)
+                cv2.imwrite(os.path.join(DUMP_DIR, image_filename), image)
---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+RAW_DIR = os.path.join(DATA_DIR, 'raw')
+
+PROCESSED_DIR = os.path.join(DATA_DIR, 'processed')
